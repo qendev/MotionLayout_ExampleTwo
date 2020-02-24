5cd40add418989ab06580cae5880839219e45a3e
qendev <qenwaweru@gmail.com>
2020-02-24 13:50:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/motionlayout_exampletwo/activities/MotionStartActivity.java
@@ -0,0 +1,16 @@
+package com.example.motionlayout_exampletwo.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.motionlayout_exampletwo.R;
+
+public class MotionStartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motion_start);
+    }
+}
